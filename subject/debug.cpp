2df17bb222629acbd09be34ba9dee28a1afd4550
叶赫那拉 <57489474+1952560@users.noreply.github.com>
2020-10-23 00:11:40 +0800

    新增stack

--- subject/debug.cpp
+++ subject/debug.cpp
@@ -1,18 +1,8 @@
-#include"../H/Vector.h"
-
+#include"../H/Stack.h"
+#include<stack>
 using namespace std;
 
-int mian()
+int main()
 {
-  Vector<int> v;
-  for (int i = 0; i < 10; i++)
-    v.push_back(i);
-  for (int i = 0; i < 10; i++)
-    cout << v[i] << ' ';
-  cout << '\n';
-  Vector<int> vec = v;
-  for (int i = 0; i < 10; i++)
-    cout << vec[i] << ' ';
-  cout << '\n';
-  return 0;
+
 }
