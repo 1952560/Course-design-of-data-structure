8e5a16fffb6048c053876a5b9579be5657cf07b8
ajt <57489474+1952560@users.noreply.github.com>
2020-12-11 16:15:05 +0800

    debug

--- H/Sort.h
+++ H/Sort.h
@@ -2,6 +2,7 @@
 #include"Vector.h"
 #include<random>
 #include<ctime>
+#include<time.h>
 #include<vector>
 
 /*
@@ -9,6 +10,8 @@
 4.shellsort 5.quicksort
 6.heapsort 7.mergesort 8 radixsort 9 exit
 */
+
+int times;
 
 template<typename T>
 void swap(T& a, T& b) {
@@ -42,18 +45,21 @@ void bubbleSort(Vector<T>& v) {
 		for (int j = 0; j < v.size() - i - 1; j++) {
 			if (v[j] > v[j + 1]) {
 				swap<T>(v[j], v[j + 1]);
+				times++;
 			}
 		}
 	}
+	std::cout<<"The number of comparison for selection sorting is:"<<times<<'\n';
+	times=0;
 }
 
 //select_sort(2)
 //data structure-------------Vector<int>
 //worst time complexity------O(n^2)
-//optimal time complexity----O(n^2)
+//optimal time complexity----O(n)
 //average time complexity----O(n^2)
 //required auxiliary space---O(1)
-//stability------------------stable
+//stability------------------unstable
 template<typename T>
 void selectionSort(Vector<T>& v) {
 	if (v.size() <= 1)
@@ -64,9 +70,13 @@ void selectionSort(Vector<T>& v) {
 			if (v[j] < v[min_index])
 				min_index = j;
 		}
-		if (min_index != i)
+		if (min_index != i){
 			swap<T>(v[i], v[min_index]);
+			times++;
+		}
 	}
+	std::cout<<"The number of comparison for selection sorting is:"<<times<<'\n';
+	times=0;
 }
 
 //insert_sort(3)
@@ -86,16 +96,20 @@ void insertionSort(Vector<T>& v) {
 		while (j >= 0 && key < v[j]) {
 			v[j + 1] = v[j];
 			j--;
+			times++;
 		}
+		times++;
 		v[j + 1] = key;
 	}
+	std::cout<<"The number of comparison for selection sorting is:"<<times<<'\n';
+	times=0;
 }
 
 //shell_sort(4)
 //data structure-------------Vector<int>
-//worst time complexity------
+//worst time complexity------O(ns)
 //optimal time complexity----O(n)
-//average time complexity----
+//average time complexity----O(nlogn)
 //required auxiliary space---O(1)
 //stability------------------unstable
 template<typename T>
@@ -104,11 +118,14 @@ void shellSort(Vector<T>& v) {
 		for (int i = gap; i < v.size(); i++) {
 			int j = i;
 			while (j - gap >= 0 && v[j] < v[j - gap]) {
+				times++;
 				swap<T>(v[j], v[j - gap]);
 				j -= gap;
 			}
 		}
 	}
+	std::cout<<"The number of comparison for selection sorting is:"<<times<<'\n';
+	times=0;
 }
 
 //quick_sort(5)
@@ -116,7 +133,7 @@ void shellSort(Vector<T>& v) {
 //worst time complexity------O(n^2)
 //optimal time complexity----O(nlogn)
 //average time complexity----O(nlogn)
-//required auxiliary space---generally O(logn),the worst is O(n)
+//required auxiliary space---O(nlogn)
 //stability------------------unstable
 template<typename T>
 void quickSort(Vector<T>& v, int start, int end) {
@@ -127,13 +144,20 @@ void quickSort(Vector<T>& v, int start, int end) {
 		while (v[j] >= base && i < j)
 			j--;
 		v[i] = v[j];
+		times++;
 		while (v[i] <= base && i < j)
 			i++;
 		v[j] = v[i];
+		times++;
 	}
 	v[i] = base;
-	quick_sort(v, start, i - 1);
-	quick_sort(v, i + 1, end);
+	times++;
+	quickSort(v, start, i - 1);
+	quickSort(v, i + 1, end);
+	if(start==0&&end==v.size()-1){
+		std::cout<<"The number of comparison for selection sorting is:"<<times<<'\n';
+		times=0;
+	}
 }
 
 //heap_sort(6)
@@ -142,6 +166,7 @@ void fitdown(int ind, Vector<T>& v, int end) {
 	if ((ind << 1) + 1 >= end)
 		return;
 	T val1 = v[(ind << 1) + 1];
+	times++;
 	if ((ind << 1) + 2 >= end) {
 		if (v[ind] < val1) {
 			v[(ind << 1) + 1] = v[ind];
@@ -166,8 +191,8 @@ void fitdown(int ind, Vector<T>& v, int end) {
 //worst time complexity------O(nlogn)
 //optimal time complexity----O(nlogn)
 //average time complexity----O(nlogn)
-//required auxiliary space---O(n)
-//stability------------------stable
+//required auxiliary space---O(1)
+//stability------------------unstable
 template<typename T>
 void heapSort(Vector<T>& v) {
 	for (int i = (v.size() >> 1) - 1; i >= 0; i--) {
@@ -175,8 +200,11 @@ void heapSort(Vector<T>& v) {
 	}
 	for (int i = v.size() - 1; i > 0; i--) {
 		swap<T>(v[0], v[i]);
+		times++;
 		fitdown(0, v, i);
 	}
+	std::cout<<"The number of comparison for selection sorting is:"<<times<<'\n';
+	times=0;
 }
 
 //merge_sort(7)
@@ -185,7 +213,7 @@ void heapSort(Vector<T>& v) {
 //optimal time complexity----O(nlogn)
 //average time complexity----O(nlogn)
 //required auxiliary space---O(n)
-//stability------------------unstable
+//stability------------------stable
 template<typename T>
 void mergeSort(Vector<T>& v, int start, int end) {
 	if (start >= end)
@@ -193,25 +221,35 @@ void mergeSort(Vector<T>& v, int start, int end) {
 	int len = end - start, mid = (len >> 1) + start;
 	int start1 = start, end1 = mid;
 	int start2 = mid + 1, end2 = end;
-	merge_sort(v, start1, end1);
-	merge_sort(v, start2, end2);
+	mergeSort(v, start1, end1);
+	mergeSort(v, start2, end2);
 	Vector<T> reg;
-	while (start1 <= end1 && start2 <= end2)
+	while (start1 <= end1 && start2 <= end2){
+		times++;
 		reg.push_back(v[start1] < v[start2] ? v[start1++] : v[start2++]);
-	while (start1 <= end1)
+	}
+	while (start1 <= end1){
+		times++;
 		reg.push_back(v[start1++]);
-	while (start2 <= end2)
+	}
+	while (start2 <= end2){
+		times++;
 		reg.push_back(v[start2++]);
-	for (int i = 0; i < reg.size(); i++)
-		v[start + i] = reg[i];
+	}
+	for(int i=0;i<reg.size();i++)
+	v[start + i] = reg[i];
+	if(start==0&&end==v.size()-1){
+		std::cout<<"The number of comparison for selection sorting is:"<<times<<'\n';
+		times=0;
+	}
 }
 
 //radix_sort(8)
 //data structure-------------Vector<int>
-//worst time complexity------O(n^2)
-//optimal time complexity----O(2n*d)
-//average time complexity----O(n)
-//required auxiliary space---O(n+k)
+//worst time complexity------O(n*d)
+//optimal time complexity----O(n*d)
+//average time complexity----O(n*d)
+//required auxiliary space---O()d              
 //stability------------------stable
 void radixSort(Vector<int> &v){
     int len = v.size();
@@ -227,9 +265,11 @@ void radixSort(Vector<int> &v){
 			if(v[i]<0)
 				index=9-abs(v[i])%res/div;
             counter[index].push_back(v[i]);
+			times++;
         }
         int radix = 0, index = 0;
         for(int i = 0; i < len;){
+			times++;
 			for(;index<counter[radix].size();index++){
 				v[i+index]=counter[radix][index];
 			}
@@ -242,4 +282,6 @@ void radixSort(Vector<int> &v){
         }
         res *= 10; div *= 10;
     }
+	std::cout<<"The number of comparison for selection sorting is:"<<times<<'\n';
+	times=0;
 }
--- subject/p10.cpp
+++ subject/p10.cpp
@@ -15,45 +15,106 @@ bool checkCin(){
 
 void instructions(){
 	std::string str[12];
-	str[0]="**			sorting algorithm comparison					**";
-	str[1]="=============================================";
-	str[2]="**		    1---bubblesort			        **";
-	str[3]="**			2---selectionsort		        **";
-	str[4]="**			3---insertionsort		        **";
-	str[5]="**			4---shellsort	           	    **";
-	str[6]="**			5---quicksort	             	**";
-	str[7]="**			6---heapsort				    **";
-	str[8]="**			7---heapsort					**";
-	str[9]="**			8---radixsort	 				**";
-	str[10]="**			9---quiting the system			**";
-	str[11]="=============================================";
+	str[0]="**         sorting algorithm comparison     **";
+	str[1]="==============================================";
+	str[2]="**          1---bubblesort                  **";
+	str[3]="**          2---selectionsort               **";
+	str[4]="**          3---insertionsort               **";
+	str[5]="**          4---shellsort                   **";
+	str[6]="**          5---quicksort                   **";
+	str[7]="**          6---heapsort                    **";
+	str[8]="**          7---heapsort                    **";
+	str[9]="**          8---radixsort                   **";
+	str[10]="**          9---quiting the system          **";
+	str[11]="==============================================";
 	for(int i=0;i<12;i++)
 		std::cout<<str[i]<<'\n';
 }
 
 int main() {
 	instructions();
-	std::cout<<"请输入要产生的随机数的个数";
+	std::cout<<"please enter the number of random numbers to be generated:";
 	int random_num=0;
 	std::cin>>random_num;
 	while(!checkCin()){
 		std::cin>>random_num;
 	}
-	Vector<int> vec = get_random(random_num);
+	Vector<int> v = get_random(random_num);
 	int order=0;
 	while(true){
-		std::cout<<std::setw(15)<<"请选择排序算法";
+		std::cout<<std::setw(15)<<"please select the sorting algorithm:";
 		std::cin>>order;
 		while(!checkCin()||order<1||order>9){
 			std::cin>>order;
 		}
+		Vector<int> v_copy=v;
+		clock_t start,end;
+		double cost;
 		switch (order)
 		{
 		case 1:
-			time(&start);
-			std::cout<<std::setw(15)<<"冒泡排序所用时间";
+			start=clock();
+			bubbleSort(v_copy);
+			end=clock();
+			cost=end-start;
+			std::cout<<std::setw(15)<<"bubblesort takes time:"<<cost<<"ms"<<'\n';
 			break;
-		
+		case 2:
+			start=clock();
+			selectionSort(v_copy);
+			end=clock();
+			cost=end-start;
+			std::cout<<std::setw(15)<<"selectionsort takes time:"<<cost<<"ms"<<'\n';
+			break;		
+		case 3:
+			start=clock();
+			insertionSort(v_copy);
+			end=clock();
+			cost=end-start;
+			std::cout<<std::setw(15)<<"insertionsort takes time:"<<cost<<"ms"<<'\n';
+			break;
+		case 4:
+			start=clock();
+			shellSort(v_copy);
+			end=clock();
+			cost=end-start;                                                                                                                                                                                                                               
+			std::cout<<std::setw(15)<<"shellsort takes time:"<<cost<<"ms"<<'\n';                                                                                                                                                                                                                                                                                                                                                                                       
+			break;
+		case 5:
+			start=clock();
+			quickSort(v_copy,0,v_copy.size()-1);
+			end=clock();
+			cost=end-start;
+			for(int i=0;i<v_copy.size()-1;i++){
+				if(v_copy[i]>v_copy[i+1]){
+					std::cout<<"error"<<' ';
+				}
+			}
+			std::cout<<std::setw(15)<<"quicksort takes time:"<<cost<<"ms"<<'\n';
+			break;         
+		case 6:
+			start=clock();
+			heapSort(v_copy);
+			end=clock();
+			cost=end-start;
+			std::cout<<std::setw(15)<<"heapsort takes time:"<<cost<<"ms"<<'\n';
+			break;
+		case 7:
+			start=clock();
+			mergeSort(v_copy,0,v_copy.size()-1);
+			end=clock();
+			cost=end-start;
+			std::cout<<std::setw(15)<<"mergesort takes time:"<<cost<<"ms"<<'\n';
+			break;
+		case 8:
+			start=clock();
+			radixSort(v_copy);
+			end=clock();
+			cost=end-start;
+			std::cout<<std::setw(15)<<"radixsort takes time:"<<cost<<"ms"<<'\n';
+			break;
+		case 9:
+			exit(0);
 		default:
 			break;
 		}
